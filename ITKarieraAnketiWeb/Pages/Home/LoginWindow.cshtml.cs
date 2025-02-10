7c79cffe4b1dae205905e8cda6163c08cc351e30
Zdravko Petrov <zp09152006@gmail.com>
2025-02-10 19:25:25 +0200

    All pages can be viewed and accessed (../Home/(page)) + reAdded MainWindow and set it to default

---
+++ ITKarieraAnketiWeb/Pages/Home/LoginWindow.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ITKarieraAnketiWeb.Pages.Home
+{
+    public class LoginWindowModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
