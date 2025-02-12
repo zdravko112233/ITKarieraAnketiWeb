07669c1fb6f54b6381042b5194ec0e0d0b761120
Alexander Shishkov <ph03nixpr0t0k0l@gmail.com>
2025-02-12 17:43:15 +0200

    Added Login/Register view model

---
+++ ITKarieraAnketiWeb/Models/Authentication/RegisterViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ITKarieraAnketiWeb.Models.Authentication
+{
+    public class RegisterViewModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
