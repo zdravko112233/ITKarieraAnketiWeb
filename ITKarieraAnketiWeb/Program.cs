db1694f8d276d734710888cee561ba2390666d12
Alexander Shishkov <ph03nixpr0t0k0l@gmail.com>
2025-02-13 15:53:05 +0200

    renamed mainwindow to index and set it as default again because we simply gotta have such a window. Can't just throw the user into registration, what if they have an account?

--- ITKarieraAnketiWeb/Program.cs
+++ ITKarieraAnketiWeb/Program.cs
@@ -41,6 +41,6 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}"); 
 
-app.MapGet("/", () => Results.Redirect("/Home/Register"));
+app.MapGet("/", () => Results.Redirect("/Home/Index"));
 
 app.Run();
