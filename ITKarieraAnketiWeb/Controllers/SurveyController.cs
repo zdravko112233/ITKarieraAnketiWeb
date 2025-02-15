14b9bb2b8c1c2254b824376e85446c30f9f7b2b6
Alexander Shishkov <ph03nixpr0t0k0l@gmail.com>
2025-02-15 14:13:28 +0200

    added surveycontroller for surveycommands

---
+++ ITKarieraAnketiWeb/Controllers/SurveyController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ITKarieraAnketiWeb.Models.ViewModels;
+
+namespace ITKarieraAnketiWeb.Controllers
+{
+    public class SurveyController : Controller
+    {
+        public IActionResult SurveyCreator()
+        {
+            return View(new SurveyCreatorViewModel());
+        }
+    }
+}
