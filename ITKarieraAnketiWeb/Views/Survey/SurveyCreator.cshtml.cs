d2b9becac2df1e141d8eb2e15e0121844e6798dc
Alexander Shishkov <ph03nixpr0t0k0l@gmail.com>
2025-02-15 14:15:42 +0200

    new survey folder with htmls inside

---
+++ ITKarieraAnketiWeb/Views/Survey/SurveyCreator.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ITKarieraAnketiWeb.Pages.Home.Survey
+{
+    public class SurveyCreationWindowModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ ITKarieraAnketiWeb/Views/Survey/SurveyViewer.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ITKarieraAnketiWeb.Pages.Home.Survey
+{
+    public class ViewSurveyWindowModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
